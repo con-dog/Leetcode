d686e708e202861da7f0852dfe1f2a26ce93c2e4
C <75226598+con-dog@users.noreply.github.com>
2023-01-14 12:51:33 +1300

    Time: 59 ms (92.36%), Space: 41.8 MB (69.16%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let steps = 0;
+    while (num) {
+        num = num % 2 === 0 ? num / 2 : num - 1;
+        steps += 1
+    }
+    return steps
+    
+};
